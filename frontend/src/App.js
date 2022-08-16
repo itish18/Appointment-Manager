68b4642aa722e27c1f3738457418734730dc2e5f
Itish Verma <itish.v007@gmail.com>
2022-08-16 14:43:42 +0530

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 // Components
 import Header from "./components/Header.jsx";
 
 // Pages
-import Calender from "./pages/Calender.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
-import Home from "./pages/Home.jsx";
+const Calender = React.lazy(() => import("./pages/Calender.jsx"));
+const Login = React.lazy(() => import("./pages/Login.jsx"));
+const Signup = React.lazy(() => import("./pages/Signup.jsx"));
+const Home = React.lazy(() => import("./pages/Home.jsx"));
 
 function App() {
   return (
     <>
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calender" element={<Calender />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<p>There's nothing here: 404!</p>} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calender" element={<Calender />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<p>There's nothing here: 404!</p>} />
+          </Routes>
+        </Suspense>
       </main>
     </>
   );
